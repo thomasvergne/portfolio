45a4278548b7188de9dce09f18cbcee3dd071535
Thomas Vergne <contact@thomas-vergne.fr>
2025-03-30 18:32:28 +0200

    feat(components): added scrollable section

---
+++ src/components/scrollable.ts
@@ -0,0 +1,60 @@
+const scrollable = document.getElementById("scrollable");
+
+if (!scrollable) {
+  throw new Error("Scrollable element not found");
+}
+
+console.log(scrollable.clientWidth, scrollable.scrollWidth, scrollable.offsetWidth)
+
+scrollable.scrollLeft = (scrollable.scrollWidth - scrollable.clientWidth) / 2;
+
+let isScrolling = false;
+
+scrollable?.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+
+  isScrolling = true;
+  scrollable.style.cursor = "grabbing";
+});
+
+scrollable?.addEventListener("wheel", (e) => {
+  const movement = e.deltaX;
+
+  if (Math.abs(movement) < Math.abs(e.deltaY)) return;
+
+  if (movement < 0 && scrollable.scrollLeft === 0) return;
+  if (
+    movement > 0 &&
+    scrollable.scrollLeft === scrollable.scrollWidth - scrollable.clientWidth
+  )
+    return;
+
+  e.preventDefault();
+
+  scrollable.scrollLeft += movement;
+}, { passive: true });
+
+scrollable?.addEventListener("mousemove", (e) => {
+  if (!isScrolling) return;
+
+  if (e.movementX >= 0 && scrollable.scrollLeft === 0) return;
+  if (
+    e.movementX <= 0 &&
+    scrollable.scrollLeft === scrollable.scrollWidth - scrollable.clientWidth
+  )
+    return;
+
+  e.preventDefault();
+
+  scrollable.scrollLeft -= e.movementX;
+});
+
+scrollable?.addEventListener("mouseup", () => {
+  isScrolling = false;
+  scrollable.style.cursor = "grab";
+});
+
+scrollable?.addEventListener("mouseleave", () => {
+  isScrolling = false;
+  scrollable.style.cursor = "grab";
+});
