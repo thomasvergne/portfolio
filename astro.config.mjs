cd495f002967657e80a666c77ed2c80bc54fecf7
Thomas Vergne <contact@thomas-vergne.fr>
2025-04-26 15:45:36 +0200

    fix(format): fixed astro and typescript configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,64 +1,65 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
-import sectionize from '@hbsnow/rehype-sectionize';
-import tailwindcss from '@tailwindcss/vite';
 
-import icon from 'astro-icon';
-import rehypeKatex from 'rehype-katex';
-import remarkMath from 'remark-math';
-
-import compress from 'astro-compress';
-
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
+import sectionize from "@hbsnow/rehype-sectionize";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, envField } from "astro/config";
+import compress from "astro-compress";
+import icon from "astro-icon";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    rehypePlugins: [sectionize, rehypeKatex],
-    remarkPlugins: [remarkMath],
-    shikiConfig: {
-      theme: {
-        bg: '#1b1a17',
-        fg: '#e8e2d9',
-      }
-    }
-  },
+	markdown: {
+		rehypePlugins: [sectionize, rehypeKatex],
+		remarkPlugins: [remarkMath],
+		shikiConfig: {
+			theme: {
+				bg: "#1b1a17",
+				fg: "#e8e2d9",
+			},
+		},
+	},
 
-  site: 'https://thomas-vergne.fr',
+	site: "https://thomas-vergne.fr",
 
-  env: {
-    schema: {
-      EMAIL_PASSWORD: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
+	env: {
+		schema: {
+			EMAIL_PASSWORD: envField.string({
+				context: "server",
+				access: "secret",
+			}),
 
-      EMAIL_USERNAME: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
+			EMAIL_USERNAME: envField.string({
+				context: "server",
+				access: "secret",
+			}),
 
-      EMAIL_HOST: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
+			EMAIL_HOST: envField.string({
+				context: "server",
+				access: "secret",
+			}),
 
-      EMAIL_PORT: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-    },
-  },
+			EMAIL_PORT: envField.string({
+				context: "server",
+				access: "secret",
+			}),
+		},
+	},
 
-  output: 'server',
+	output: "server",
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-  integrations: [icon(), compress({
-    CSS: false,
-  })],
+	integrations: [
+		icon(),
+		compress({
+			CSS: false,
+		}),
+	],
 
-  adapter: vercel()
+	adapter: vercel(),
 });
