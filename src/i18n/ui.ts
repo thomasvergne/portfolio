73dcb2b5e61bb9033f3fdb85571983647a258059
Thomas Vergne <contact@thomas-vergne.fr>
2025-06-23 15:05:31 +0200

    feat(i18n): added and updated some translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,163 +1,181 @@
 export const ui = {
-	en: {
+  en: {
     toc: "Table of contents",
 
-		navbar: {
-			links: {
-				projects: "Projects",
-				blog: "Articles",
-				contact: "Contact",
-			},
-		},
-		header:
-			"Hey, I'm a web developer based in France. I've been passionate about coding for over 7 years now.",
+    navbar: {
+      links: {
+        projects: "Projects",
+        blog: "Articles",
+        contact: "Contact",
+      },
+    },
 
-		carousel: {
-			button: "Visit the website",
-			lca: {
-				title: "La Couture d'Amandine",
-				description: "A description of La Couture d'Amandine.",
-			},
+    header: {
+      firstPart: "hey, i'm",
+      secondPart: "thomas vergne",
+      marquee: "web developer",
+      cta: "contact me",
+    },
 
-			ledonvert: {
-				title: "Le Don Vert",
-				description: "A description of Le Don Vert.",
-			},
+    projects: {
+      title: {
+        firstPart: "some of the projects",
+        secondPart: "i've worked on",
+      },
 
-			numa: {
-				title: "Numa",
-				description: "A description of Numa.",
-			},
-		},
+      list: {
+        lca: "A website for a geek, ethical and eco-responsible sewer.",
+        ledonvert: "A website for a plant donation platform in whole France.",
+        numa: "A website that offers an emotional agenda with AI.",
+        dutoitautomobile:
+          "A website for a garage specializing in 4x4 vehicles.",
+      },
+      cta: "view project",
+    },
 
-		articles: {
-			title: "My articles",
-			description:
-				"A selection of articles I've written about programming languages, illustrated with code examples.",
-			readMore: "Read more",
-			read: "Read this article",
-		},
+    articles: {
+      title: {
+        firstPart: "i also write articles",
+        secondPart: "about everything",
+      },
+      description:
+        "A selection of articles I've written about programming languages, illustrated with code examples.",
+      readMore: "Read more",
+      read: "Read this article",
+    },
 
-		contact: {
-			title: "Contact me",
+    contact: {
+      title: {
+        firstPart: "Got an idea?",
+        secondPart: "Contact me",
+      },
 
-			socials: "My social networks",
+      socials: "My social networks",
 
-			form: {
-				name: "Your full name",
-				email: "Your email address",
-				message: "Your message",
-				send: "Send message",
-				subject: "Message subject",
-			},
+      form: {
+        name: "Your full name",
+        email: "Your email address",
+        message: "Your message",
+        send: "Send message",
+        subject: "Message subject",
+      },
 
-			success: {
-				title: "Message sent",
-				description:
-					"Thank you for your message, I will respond as soon as possible.",
-				button: "Return to home",
-			},
+      success: {
+        title: "Message sent",
+        description:
+          "Thank you for your message, I will respond as soon as possible.",
+        button: "Return to home",
+      },
 
-			error: {
-				title: "Error",
-				description:
-					"An error occurred while sending the message. Please try again.",
-				button: "Return to home",
+      error: {
+        title: "Error",
+        description:
+          "An error occurred while sending the message. Please try again.",
+        button: "Return to home",
 
-				missingData: {
-					title: "Missing data",
-					description: "Please fill in all fields of the form.",
-				},
+        missingData: {
+          title: "Missing data",
+          description: "Please fill in all fields of the form.",
+        },
 
-				invalidEmail: {
-					title: "Invalid email address",
-					description: "Please enter a valid email address.",
-				},
-			},
+        invalidEmail: {
+          title: "Invalid email address",
+          description: "Please enter a valid email address.",
+        },
+      },
 
-			back: "Return to home",
-		},
-	},
+      back: "Return to home",
+    },
+  },
 
-	fr: {
+  fr: {
     toc: "Table des matières",
 
-		navbar: {
-			links: {
-				projects: "Projets",
-				blog: "Articles",
-				contact: "Contact",
-			},
-		},
-		header:
-			"Salut, je suis un développeur web basé en France. Je suis passionné par le code depuis maintenant plus de 7 ans.",
+    navbar: {
+      links: {
+        projects: "Projets",
+        blog: "Articles",
+        contact: "Contact",
+      },
+    },
 
-		carousel: {
-			button: "Visiter le site",
-			lca: {
-				title: "La Couture d'Amandine",
-				description: "Une description de la couture d'Amandine.",
-			},
+    header: {
+      firstPart: "salut, je suis",
+      secondPart: "thomas vergne",
+      marquee: "développeur web",
+      cta: "me contacter",
+    },
 
-			ledonvert: {
-				title: "Le Don Vert",
-				description: "Une description du Don Vert.",
-			},
+    projects: {
+      title: {
+        firstPart: "certains des projets",
+        secondPart: "auxquels j'ai participés",
+      },
 
-			numa: {
-				title: "Numa",
-				description: "Une description de Numa.",
-			},
-		},
+      list: {
+        lca: "Un site pour une couturière geek, éthique et écoresponsable.",
+        ledonvert: "Un site pour une plateforme de don de plantes en France.",
+        numa: "Un site qui propose un agenda émotionnel avec IA.",
+        dutoitautomobile: "Un site pour un garage spécialisé dans les 4x4.",
+      },
 
-		articles: {
-			title: "Mes articles",
-			description:
-				"Une sélection d'articles que j'ai écrits sur les langages de programmation, illustrés par des exemples de code.",
-			readMore: "En savoir plus",
-			read: "Lire cet article",
-		},
+      cta: "voir le projet",
+    },
 
-		contact: {
-			title: "Me contacter",
+    articles: {
+      title: {
+        firstPart: "j'écris aussi des articles",
+        secondPart: "à propos de tout"
+      },
+      description:
+        "Une sélection d'articles que j'ai écrits sur les langages de programmation, illustrés par des exemples de code.",
+      readMore: "En savoir plus",
+      read: "Lire cet article",
+    },
 
-			socials: "Mes réseaux sociaux",
+    contact: {
+      title: {
+        firstPart: "Vous avez une idée ?",
+        secondPart: "Contactez-moi",
+      },
 
-			form: {
-				name: "Votre nom et prénom",
-				email: "Votre adresse e-mail",
-				message: "Votre message",
-				send: "Envoyer le message",
-				subject: "Sujet du message",
-			},
+      socials: "Mes réseaux sociaux",
 
-			success: {
-				title: "Message envoyé",
-				description:
-					"Merci pour votre message, je vous répondrai dès que possible.",
-				button: "Retourner à l'accueil",
-			},
+      form: {
+        name: "Votre nom et prénom",
+        email: "Votre adresse e-mail",
+        message: "Votre message",
+        send: "Envoyer le message",
+        subject: "Sujet du message",
+      },
 
-			error: {
-				title: "Erreur",
-				description:
-					"Une erreur s'est produite lors de l'envoi du message. Veuillez réessayer.",
-				button: "Retourner à l'accueil",
+      success: {
+        title: "Message envoyé",
+        description:
+          "Merci pour votre message, je vous répondrai dès que possible.",
+        button: "Retourner à l'accueil",
+      },
 
-				missingData: {
-					title: "Données manquantes",
-					description: "Veuillez remplir tous les champs du formulaire.",
-				},
+      error: {
+        title: "Erreur",
+        description:
+          "Une erreur s'est produite lors de l'envoi du message. Veuillez réessayer.",
+        button: "Retourner à l'accueil",
 
-				invalidEmail: {
-					title: "Adresse e-mail invalide",
-					description: "Veuillez entrer une adresse e-mail valide.",
-				},
-			},
+        missingData: {
+          title: "Données manquantes",
+          description: "Veuillez remplir tous les champs du formulaire.",
+        },
 
-			back: "Retourner à l'accueil",
-		},
-	},
+        invalidEmail: {
+          title: "Adresse e-mail invalide",
+          description: "Veuillez entrer une adresse e-mail valide.",
+        },
+      },
+
+      back: "Retourner à l'accueil",
+    },
+  },
 } as const;
 
 export const languagesNames: Record<Languages, [string, string]> = {
