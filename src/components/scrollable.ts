f7f013e7eeee386ec8c4387b26b915d941506dbf
Thomas Vergne <contact@thomas-vergne.fr>
2025-06-26 17:06:29 +0200

    fix(components): scrollable component now allows multiple instances

--- src/components/scrollable.ts
+++ src/components/scrollable.ts
@@ -1,58 +1,56 @@
-const scrollable = document.querySelector<HTMLDivElement>(".scrollable");
+const scrollables = document.querySelectorAll<HTMLDivElement>(".scrollable");
 
-if (!scrollable) {
-	throw new Error("Scrollable element not found");
-}
+scrollables.forEach((scrollable) => {
+  scrollable.scrollLeft = (scrollable.scrollWidth - scrollable.clientWidth) / 2;
 
-scrollable.scrollLeft = (scrollable.scrollWidth - scrollable.clientWidth) / 2;
+  let isScrolling = false;
 
-let isScrolling = false;
+  scrollable?.addEventListener("mousedown", (e) => {
+    e.preventDefault();
 
-scrollable?.addEventListener("mousedown", (e) => {
-	e.preventDefault();
+    isScrolling = true;
+    scrollable.style.cursor = "grabbing";
+  });
 
-	isScrolling = true;
-	scrollable.style.cursor = "grabbing";
-});
+  scrollable?.addEventListener("wheel", (e) => {
+    const movement = e.deltaX;
 
-scrollable?.addEventListener("wheel", (e) => {
-	const movement = e.deltaX;
+    if (Math.abs(movement) < Math.abs(e.deltaY)) return;
 
-	if (Math.abs(movement) < Math.abs(e.deltaY)) return;
+    if (movement < 0 && scrollable.scrollLeft === 0) return;
+    if (
+      movement > 0 &&
+      scrollable.scrollLeft === scrollable.scrollWidth - scrollable.clientWidth
+    )
+      return;
 
-	if (movement < 0 && scrollable.scrollLeft === 0) return;
-	if (
-		movement > 0 &&
-		scrollable.scrollLeft === scrollable.scrollWidth - scrollable.clientWidth
-	)
-		return;
+    e.preventDefault();
 
-	e.preventDefault();
+    scrollable.scrollLeft += movement;
+  });
 
-	scrollable.scrollLeft += movement;
-});
+  scrollable?.addEventListener("mousemove", (e) => {
+    if (!isScrolling) return;
 
-scrollable?.addEventListener("mousemove", (e) => {
-	if (!isScrolling) return;
+    if (e.movementX >= 0 && scrollable.scrollLeft === 0) return;
+    if (
+      e.movementX <= 0 &&
+      scrollable.scrollLeft === scrollable.scrollWidth - scrollable.clientWidth
+    )
+      return;
 
-	if (e.movementX >= 0 && scrollable.scrollLeft === 0) return;
-	if (
-		e.movementX <= 0 &&
-		scrollable.scrollLeft === scrollable.scrollWidth - scrollable.clientWidth
-	)
-		return;
+    e.preventDefault();
 
-	e.preventDefault();
+    scrollable.scrollLeft -= e.movementX;
+  });
 
-	scrollable.scrollLeft -= e.movementX;
-});
+  scrollable?.addEventListener("mouseup", () => {
+    isScrolling = false;
+    scrollable.style.cursor = "grab";
+  });
 
-scrollable?.addEventListener("mouseup", () => {
-	isScrolling = false;
-	scrollable.style.cursor = "grab";
-});
-
-scrollable?.addEventListener("mouseleave", () => {
-	isScrolling = false;
-	scrollable.style.cursor = "grab";
+  scrollable?.addEventListener("mouseleave", () => {
+    isScrolling = false;
+    scrollable.style.cursor = "grab";
+  });
 });
